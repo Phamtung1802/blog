74245330eeeec2204092da5767f7e77752f60098
Pham Tung <phamtung.0218@gmail.com>
2022-03-28 13:28:20 +0700

    finish layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './template/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss','./_var.scss'],
 })
 export class AppComponent {
   title = 'blog';
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './template/header/header.component';
+import { LeftSideComponent } from './template/left-side/left-side.component';
+import { RightSideComponent } from './template/right-side/right-side.component';
+import { MainBodyComponent } from './template/main-body/main-body.component';
+import { FooterComponent } from './template/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LeftSideComponent,
+    RightSideComponent,
+    MainBodyComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
