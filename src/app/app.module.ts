ef62e019280509eca39d00d84dae93cc2f636bf0
Pham Tung <phamtung.0218@gmail.com>
2022-04-30 12:57:43 +0700

    set up layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { ThemeService } from 'src/services/theme.service';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopMenuComponent } from './template/top-menu/top-menu.component';
+import { MainBodyComponent } from './template/main-body/main-body.component';
+import { LeftSideBarComponent } from './template/left-side-bar/left-side-bar.component';
+import { RightSideBarComponent } from './template/right-side-bar/right-side-bar.component';
+import { FooterComponent } from './template/footer/footer.component';
 @NgModule({
   declarations: [
     AppComponent,
-    TopMenuComponent
+    TopMenuComponent,
+    MainBodyComponent,
+    LeftSideBarComponent,
+    RightSideBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
